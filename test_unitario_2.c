792517001a9eb60f8cfe938d130d3ffed2ab1dc8
kasimxo <kasimxo@gmail.com>
2023-11-25 18:25:27 +0100

    Modificado el Test_2
    Ahora evalua correctamente si el resultado de la ejecución es correcto o no

--- test_unitario_2.c
+++ test_unitario_2.c
@@ -7,6 +7,11 @@
 
 #define MAX_LINE 80
 
+//Declaramos esta variable para poder guardar el resultado del proceso hijo
+int status;
+//Declaramos la función de ejecutarComando
+int ejecutarComando();
+
 int ejecutarComando(char* comando) {
     pid_t pid = fork();
 
@@ -21,28 +26,56 @@ int ejecutarComando(char* comando) {
             i++;
         }
         args[i] = NULL;
+	
+	/*
+	Original:
+	execvp(args[0], args);
+	exit(0);
+	*/
 
-        execvp(args[0], args);
-        exit(0);
+	//Guardamos el resultado de la ejecución del comando
+        int resultado = execvp(args[0], args);
+	//Evaluamos si el resultado de la ejecución del comando ha sido correcta o no
+	if(resultado == 0) {
+		//Si la ejecución del comando ha salido bien devolvemos 0
+		exit(0);
+	} else {
+		//Si ha fallado devolvemos un código de error
+		exit(-1);
+	}
     } else if (pid > 0) {
-        wait(NULL);
+	/*
+	ORIGINAL:
+	wait(NULL);
+	Este código espera a que el proceso hijo termine, pero ignora completamente el resultado de ese proceso hijo
+	*/
+	//Aquí esperamos a que el proceso hijo termine, pero además guardamos el resultado en una variable
+	waitpid(pid,&status, 0);
+	//Devolvemos el resultado del proceso hijo
+	return status;
     } else {
         return -1; // Error al crear el proceso hijo
     }
-
+	
     return 0; // Éxito
 }
 
 int main() {
-    char comando_inexistente[] = "xyzabc";
-    int resultado = ejecutarComando(comando_inexistente);
+    	//char comando_inexistente[] = "xyzabc";
+    	char comando_inexistente[] = "datxxxxxe";
+    	int resultado = ejecutarComando(comando_inexistente);
+    	/*
+	ORIGINAL:
+	if (resultado == -1) {
+    	Esta línea pretende evaluar si hemos recibido un código de error pero no funciona correctamente
+	*/
+	//Si todo ha ido bien el resultado será 0, por lo que evaluamos si ha habido algún error:
+	if (resultado != 0) {
+        	printf("Prueba 2: Pasada - Error al ejecutar comando inexistente.\n");
+    	} else {
+        	printf("Prueba 2: Fallida - El comando inexistente se ejecutó correctamente.\n");
+    	}
 
-    if (resultado == -1) {
-        printf("Prueba 2: Pasada - Error al ejecutar comando inexistente.\n");
-    } else {
-        printf("Prueba 2: Fallida - El comando inexistente se ejecutó correctamente.\n");
-    }
-
-    return 0;
+    	return 0;
 }
 
